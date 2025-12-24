5e53085ed7b9eb7ea86f3883183d9a08577b9bdd
bytes1 <bytes111@gmail.com>
2025-12-24 12:26:51 +0200

    feat(board): implemented board repository

---
+++ packages/core/src/entity/board.ts
@@ -0,0 +1,29 @@
+export interface Board {
+  id: string;
+  projectId: string;
+  name: string;
+  isDefault: boolean;
+  position: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface BoardList {
+  id: string;
+  boardId: string;
+  name: string;
+  position: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface BoardListItem {
+  id: string;
+  boardListId: string;
+  title: string;
+  description: string;
+  position: number;
+  createdAt: string;
+  updatedAt: string;
+  completedAt: string;
+}
--- packages/core/src/repositories/index.ts
+++ packages/core/src/repositories/index.ts
@@ -1,3 +1,4 @@
 export * from "./account-repository";
+export * from "./board-repository";
 export * from "./project-repository";
 export * from "./user-repository";

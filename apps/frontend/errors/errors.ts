b8f0aed6a975cddb5ee7ed50b3f68c14a7a7e5d6
bytes1 <bytes111@gmail.com>
2026-02-25 12:23:01 +0200

    feat(frontend): Implement app error

--- apps/frontend/errors/errors.ts
+++ apps/frontend/errors/errors.ts
@@ -1,17 +1,95 @@
-import { AccountRegisterMessage } from "shared/utils";
+import { ApiErrorResponse, ApiResponse } from "apisauce";
 
-export class ServerError extends Error {
-  constructor(message: string, cause?: Error) {
-    super(`Something went wrong with the server: ${message}`);
-  }
+export enum AppErrorCode {
+  NetworkUnreachable = "NETWORK_UNREACHABLE",
+  RequestTimeout = "REQUEST_TIMEOUT",
+  ServerFailure = "SERVER_FAILURE",
+  UnknownFailure = "UNKNOWN_FAILURE",
+  EmptyResponseData = "EMPTY_RESPONSE_DATA"
 }
 
-export class UserError extends Error {
-  constructor(message: string, cause?: Error) {
-    super(message, { cause });
+export type AppErrorDetails = {
+  code: AppErrorCode;
+  message: string;
+  retryable: boolean;
+  status?: number;
+  cause?: any;
+};
+
+const getAppErrorType = (response: ApiResponse<any> | null) => {
+  const problem = response?.problem;
+
+  if (problem === "TIMEOUT_ERROR") {
+    return AppErrorCode.RequestTimeout;
+  }
+
+  if (problem === "NETWORK_ERROR") {
+    return AppErrorCode.NetworkUnreachable;
+  }
+
+  if (problem === "SERVER_ERROR") {
+    return AppErrorCode.ServerFailure;
+  }
+
+  if (problem === "UNKNOWN_ERROR" || (!response?.ok && !response?.data)) {
+    return AppErrorCode.UnknownFailure;
+  }
+
+  if (response.ok && !response.data && response.status !== 204) {
+    return AppErrorCode.EmptyResponseData;
+  }
+
+  return null;
+};
+
+export const createNetworkAppError = (
+  response: ApiResponse<any>
+): AppError | null => {
+  const appErrorCode = getAppErrorType(response);
+
+  if (!appErrorCode) {
+    return null;
+  }
+
+  const appErrorDetails: Record<AppErrorCode, Omit<AppErrorDetails, "code">> = {
+    [AppErrorCode.RequestTimeout]: {
+      retryable: true,
+      message: "The request took too long. Check your connection and try again."
+    },
+    [AppErrorCode.NetworkUnreachable]: {
+      retryable: true,
+      message: "No network connection. Please reconnect and retry."
+    },
+    [AppErrorCode.ServerFailure]: {
+      retryable: true,
+      message: "We hit a server error. Please try again in a moment."
+    },
+    [AppErrorCode.UnknownFailure]: {
+      retryable: false,
+      message: "Something went wrong. Please try again later."
+    },
+    [AppErrorCode.EmptyResponseData]: {
+      retryable: false,
+      message: "The server returned an empty response. Please try again later."
+    }
+  };
+
+  return new AppError({
+    code: appErrorCode,
+    ...appErrorDetails[appErrorCode],
+    cause: response?.data,
+    status: response.status
+  });
+};
+
+export class AppError extends Error {
+  public appErrorDetails: AppErrorDetails;
+
+  constructor(appErrorDetails: AppErrorDetails) {
+    super(appErrorDetails.message, {
+      cause: appErrorDetails.cause
+    });
+
+    this.appErrorDetails = appErrorDetails;
   }
 }
-
-export const AccountAlreadyExists = new UserError(
-  AccountRegisterMessage.alreadyExist
-);
--- apps/frontend/store/account/api/auth.api.ts
+++ apps/frontend/store/account/api/auth.api.ts
@@ -1,25 +1,23 @@
 import { ApisauceInstance } from "apisauce";
 import { AuthCredentials, RegisterResponse } from "./dtos/AuthDto";
-import { ServerError } from "@/errors/errors";
+import { createNetworkAppError } from "@/errors/errors";
 
 export const createAuthApi = (sauce: ApisauceInstance) => {
   const postRegister = async (
     payload: AuthCredentials
   ): Promise<RegisterResponse> => {
-    const { data, ok } = await sauce.post<RegisterResponse>(
+    const response = await sauce.post<RegisterResponse>(
       "/auth/register",
       payload
     );
 
-    if (!ok && data) {
-      throw new ServerError(data.message);
+    const appNetworkError = createNetworkAppError(response);
+
+    if (appNetworkError) {
+      throw appNetworkError;
     }
 
-    if (!data) {
-      throw new ServerError("Empty data");
-    }
-
-    return data;
+    return response.data!;
   };
 
   return {
